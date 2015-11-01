3475805239adb0be2a9da4777a7ddd86dc87188b
dave <dave@avanthay.ch>
2015-11-01 11:01:21 +0100

    #1 create page layout and admin page layout with navigation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,22 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="app")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('::index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/employee", name="employee")
+     */
+    public function adminEmployeeAction() {
+        return $this->render(':admin:employee_overview.html.twig');
     }
 }
