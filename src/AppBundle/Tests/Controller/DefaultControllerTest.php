9c84778188064e472e98120f9bc268484fa1f3e0
dave <dave@avanthay.ch>
2015-11-07 15:15:59 +0100

    add composer phpunit/phpunit to the project to remove this dependency and get support while creating testcases

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,15 +4,14 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
+class DefaultControllerTest extends WebTestCase {
+
+    public function testIndex() {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        /* client should be redirected to the login screen */
+        $this->assertTrue($client->getResponse()->isRedirect());
     }
 }
